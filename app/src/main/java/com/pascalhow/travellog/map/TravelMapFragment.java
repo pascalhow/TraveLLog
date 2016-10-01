ac412aa5c2ff3953548158b33600677c22a1746d
pascalhow <pascal.how@mydrivesolutions.com>
2016-10-01 23:50:42 +0100

    Added gradle dependencies libraries for google maps Separate dependency libraries version

---
+++ app/src/main/java/com/pascalhow/travellog/map/TravelMapFragment.java
@@ -0,0 +1,19 @@
+package com.pascalhow.travellog.map;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.LatLngBounds.Builder;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+/**
+ * Created by pascal on 01/10/2016.
+ */
+
+public class TravelMapFragment extends SupportMapFragment {
+}
